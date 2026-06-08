e4fa21962ed3046d7ac6505c300a79b591762e12
Yingzhi Zhang <91544323+Rocky1salady-killer@users.noreply.github.com>
2026-06-08 16:01:00 +0900

    Implement model export from specified directories
    Added functionality to export models from specified directories, handling file existence and exceptions.

--- exportmodel.py
+++ exportmodel.py
@@ -1,7 +1,32 @@
 import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 from ultralytics import YOLO
 
-model = YOLO("weights\ZiLOBase-coco.pt")  
-model.train
-model.export(format="engine", half=True)  
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
+model_dirs = [
+    "yoursmodel",
+]
+
+
+for model_dir in model_dirs:
+    pt_path = os.path.join(model_dir, "best.pt")
+    if not os.path.exists(pt_path):
+        print(f"❌ Skipping {pt_path}: file not found.")
+        continue
+
+    print(f"\n🚀 Exporting model: {pt_path}")
+    try:
+        model = YOLO(pt_path)
+        model.export(
+            format="engine",
+    	    dynamic=False,
+            imgsz=640,
+            batch=1,
+            workspace=4,
+            half=True,         # FP16
+            device=0
+        )
+        print(f"✅ Exported: {pt_path}")
+    except Exception as e:
+        print(f"❌ Failed to export {pt_path}: {e}")
